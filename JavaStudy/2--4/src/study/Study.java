3ca41110cd0de2f2b9b4d8a2780d032455baafa2
mai3373 <137670006+mai3373@users.noreply.github.com>
2023-09-23 22:26:55 +0900

    課題2-4

---
+++ JavaStudy/2--4/src/study/Study.java
@@ -0,0 +1,31 @@
+package study;
+//①FruitsConstants.javaをインポートしなさい。
+import study.constants.FruitsConstants;
+//②Fruits.javaをインポートしなさい。
+import study.fruits.Fruits;
+	//②Fruits.javaをインポートしなさい。
+
+	/**
+	 * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+	 *
+	 * 問①〜問③まであります。
+	 * 全て回答し、課題にある画像と同じ表示になるようにしてください。
+	 *
+	 */
+public class Study {
+
+	    public static void main(String[] args) {
+
+	        /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+	         *     また、課題の画像と同じ表示になるように
+	         *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
+	         */
+	    	String fruits = FRUITS_LEMON_04;
+	    	int count =  FRUITS_COUNT_20;
+	    	
+	    	
+	    	printFruits(fruits, count);
+
+	    }
+	
+}
