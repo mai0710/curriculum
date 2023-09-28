c6ac226b6372c14dd6ef5092f754c794c619d88d
mai3373 <137670006+mai3373@users.noreply.github.com>
2023-09-28 20:07:39 +0900

    課題Check2

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -4,15 +4,19 @@ import constants.Constants;
 
 public class Check {
 	
-
+	private  String firstName = "舞";
+	private  String lastName ="須藤";
+	
+	public void pl(){
+		System.out.println(firstName + lastName);
+	}
+	
 	public static void main(String[] args) { 
 		
-		final  String firstName = "舞";
-		final  String lastName = "須藤";
-
-	    System.out.println(firstName + lastName);
-	    
-	    
+		Check woman = new Check();
+		woman.pl();
+	
+		
 	    Pet pet1 = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 	    RobotPet RP1 = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
 	    
