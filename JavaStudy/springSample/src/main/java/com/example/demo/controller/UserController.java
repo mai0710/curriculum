bdce85c49474e95badc7f313cc933a6ae690a9cf
mai3373 <137670006+mai3373@users.noreply.github.com>
2023-11-19 02:35:56 +0900

    springSample課題

---
+++ JavaStudy/springSample/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.UserService;
+/**
+ * ユーザー情報 Controller
+ */
+@Controller
+
+public class UserController {
+	 /**
+	   * ユーザー情報 Service
+	   */
+	  //2行追加
+	@Autowired
+    private UserService userService; 
+	  /**
+	   * ユーザー情報一覧画面を表示
+	   * @param  model Model
+	   * @return  ユーザー情報一覧画面のHTML
+	   */
+	  @RequestMapping("/user/list")
+		public String displayList(Model model) {
+
+	    //3行追加
+		  // バックエンドからユーザー情報を取得
+	        List<User> userList = userService.getAllUsers();
+
+	        // モデルにユーザーリストをセット
+	        model.addAttribute("users", userList);
+
+	        return "user/list"; // ユーザー情報一覧画面のHTMLテンプレートの名前
+
+
+
+	  }
+}
---
+++ JavaStudy/springSample/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ユーザー情報 Repository
+ */
+@Repository
+//1行追加
+public class UserRepository extends JpaRepository<User, Long>{
+
+}
