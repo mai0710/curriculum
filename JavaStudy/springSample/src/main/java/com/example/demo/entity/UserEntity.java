4d7b1960c639a59a404209f8dd2042e0a8aa07dd
mai3373 <137670006+mai3373@users.noreply.github.com>
2023-12-06 21:54:13 +0900

    springSample課題

--- JavaStudy/springSample/src/main/java/com/example/demo/entity/UserEntity.java
+++ JavaStudy/springSample/src/main/java/com/example/demo/entity/UserEntity.java
@@ -2,20 +2,17 @@ package com.example.demo.entity;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
 /**
  * ユーザー情報 Entity
  */
 @Entity
 @Data
-@Table(name = "user")
+@Table(name = "user" , schema = "public")
 public class UserEntity  {
 
 	/**
