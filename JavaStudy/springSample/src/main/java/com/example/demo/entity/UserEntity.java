4d6cf104e8729b5b657f8763931a57e277707a8e
mai3373 <137670006+mai3373@users.noreply.github.com>
2023-12-07 21:27:29 +0900

    springSample

--- JavaStudy/springSample/src/main/java/com/example/demo/entity/UserEntity.java
+++ JavaStudy/springSample/src/main/java/com/example/demo/entity/UserEntity.java
@@ -2,10 +2,14 @@ package com.example.demo.entity;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 /**
  * ユーザー情報 Entity
