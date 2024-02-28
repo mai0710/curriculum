207735e096f3ad24ba2ede21524e241db428bffc
mai3373 <137670006+mai3373@users.noreply.github.com>
2024-02-28 19:56:02 +0900

    １

--- JavaStudy/springSample6/src/main/java/com/example/demo/SpringSampleApplication.java
+++ JavaStudy/springSample6/src/main/java/com/example/demo/SpringSampleApplication.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSampleApplication.class, args);
+		SpringSampleApplication.run(SpringSampleApplication.class, args);
 	}
 
 }
